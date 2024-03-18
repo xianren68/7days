16d297104a2f7e96ca69331c3911321136c9b286
xianren <xianren711@qq.com>
2024-03-18 18:15:45 +0800

    day3

--- Cache/cache_test.go
+++ Cache/cache_test.go
@@ -3,6 +3,7 @@ package Cache
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"reflect"
 	"testing"
 )
@@ -51,4 +52,20 @@ func TestGet(t *testing.T) {
 	if view, err := gee.Get("unknown"); err == nil {
 		t.Fatalf("the value of unknow should be empty, but %s got", view)
 	}
+}
+
+func TestHttp(t *testing.T) {
+	NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := NewHttpPool(addr)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
 }
---
+++ Cache/http.go
@@ -0,0 +1,56 @@
+package Cache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const DEFAULT_BASE_PATH = "/_cache/"
+
+// HttpPool Http client pool.
+type HttpPool struct {
+	// host:port
+	self string
+	// request path
+	basePath string
+}
+
+func NewHttpPool(self string) *HttpPool {
+	return &HttpPool{
+		self:     self,
+		basePath: DEFAULT_BASE_PATH,
+	}
+}
+func (p *HttpPool) Log(format string, v ...any) {
+	log.Printf("[Server %s] %s\n", p.self, fmt.Sprintf(format, v...))
+}
+func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	// cache group.
+	groupName := parts[0]
+	// the key of request data.
+	key := parts[1]
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: ", http.StatusNotFound)
+		return
+	}
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.Byteslice())
+}
